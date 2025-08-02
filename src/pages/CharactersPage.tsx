f50e973f2d9e0116494a417c93beb71e0f6604d0
Eugene <eugenelevkovych@yahoo.com>
2025-08-02 13:30:50 +0300

    add type character

--- src/pages/CharactersPage.tsx
+++ src/pages/CharactersPage.tsx
@@ -10,14 +10,44 @@ import { handleLoadMore } from "../utils/index.ts";
 import FiltersOverlay from "../components/FiltersOverlay.tsx";
 import Card from "../components/Card.tsx";
 
+type Character = {
+  id: number;
+  name: string;
+  status: string;
+  species: string;
+  type: string;
+  gender: string;
+  origin: { name: string; url: string };
+  location: { name: string; url: string };
+  image: string;
+  episode: string[];
+  url: string;
+  created: string;
+};
+
+type ApiInfo = {
+  count: number;
+  pages: number;
+  next: string | null;
+  prev: string | null;
+};
+
+type CharactersApiResponse = {
+  info: ApiInfo;
+  results: Character[];
+};
+
 export default function CharactersPage() {
   const [search, setSearch] = useState("");
-  const [charactersData, setCharactersData] = useState(null);
+  const [charactersData, setCharactersData] =
+    useState<CharactersApiResponse | null>(null);
   const [pageNumber, setPageNumber] = useState(1);
   const [error, setError] = useState(null);
   const [species, setSpecies] = useState("");
   const [gender, setGender] = useState("");
   const [status, setStatus] = useState("");
+
+  console.log(charactersData);
 
   const selectFilters = getCharactersFilters({
     species,
