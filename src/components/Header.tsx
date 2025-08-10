e5511a087a872a39f7091f6c3aca1909441db846
Eugene <eugenelevkovych@yahoo.com>
2025-08-10 14:55:11 +0300

    add path

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,13 +1,13 @@
-import { NavLink } from 'react-router';
-import { useState } from 'react';
-import logoBlack from '../assets/images/logo-black.png';
-import MobileMenu from './MobileMenu';
+import { NavLink } from "react-router";
+import { useState } from "react";
+import logoBlack from "../assets/images/logo-black.png";
+import MobileMenu from "./MobileMenu";
 
 export default function Header() {
   const [isOpen, setIsOpen] = useState(false);
 
   const toggleMenu = () => {
-    setIsOpen(open => !open);
+    setIsOpen((open) => !open);
   };
   return (
     <div className="fixed z-50 w-full border-b-0 shadow-md py-[6px] bg-white">
@@ -15,12 +15,12 @@ export default function Header() {
       <div className="container flex justify-between items-center container-centered">
         <img src={logoBlack} alt="logo" />
         <div className="hidden md:flex gap-6">
-          <NavLink to={'/'}>Characters</NavLink>
-          <NavLink to={'locations'}>Locations</NavLink>
-          <NavLink to={'episodes'}>Episodes</NavLink>
+          <NavLink to={"/"}>Characters</NavLink>
+          <NavLink to={"locations"}>Locations</NavLink>
+          <NavLink to={"episodes"}>Episodes</NavLink>
         </div>
         <svg onClick={toggleMenu} className="md:hidden size-6 fill-gray4">
-          <use href="./sprite.svg#icon-burger1"></use>
+          <use href="/sprite.svg#icon-burger1"></use>
         </svg>
       </div>
     </div>
