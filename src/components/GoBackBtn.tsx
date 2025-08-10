db1274cc51e92a8d7804bb40ef871935c2aace28
Eugene <eugenelevkovych@yahoo.com>
2025-08-10 21:40:13 +0300

    test 7

--- src/components/GoBackBtn.tsx
+++ src/components/GoBackBtn.tsx
@@ -1,17 +1,18 @@
-// import { NavLink } from "react-router";
+import { NavLink } from "react-router";
 
-// type GoBackBtnProps = {
-//   to: string;
-// };
+type GoBackBtnProps = {
+  to: string;
+};
 
-// export default function GoBackBtn({ to }: GoBackBtnProps) {
-export default function GoBackBtn() {
+export default function GoBackBtn({ to }: GoBackBtnProps) {
   return (
-    <div className="flex items-center gap-2 font-bold text-lg text-black uppercase cursor-pointer">
-      <svg className="size-6">
-        <use xlinkHref="/sprite.svg#icon-arrow-back"></use>
-      </svg>
-      <p>Go back</p>
-    </div>
+    <NavLink to={to}>
+      <div className="flex items-center gap-2 font-bold text-lg text-black uppercase cursor-pointer">
+        <svg className="size-6">
+          <use xlinkHref="/sprite.svg#icon-arrow-back"></use>
+        </svg>
+        <p>Go back</p>
+      </div>
+    </NavLink>
   );
 }
--- src/pages/LocationDetailsPage.tsx
+++ src/pages/LocationDetailsPage.tsx
@@ -6,7 +6,7 @@ import { API_URL } from "../data/api.js";
 import Card from "../components/Card.js";
 import type { Character, Location } from "../types/api.ts";
 import ErrorPage from "./ErrorPage.tsx";
-// import GoBackBtn from "../components/GoBackBtn.tsx";
+import GoBackBtn from "../components/GoBackBtn.tsx";
 import Loader from "../components/Loader.tsx";
 
 export default function LocationDetailsPage() {
@@ -72,7 +72,7 @@ export default function LocationDetailsPage() {
     <>
       <div className="container pt-21 pb-20.5 md:pt-22.5 md:pb-9 container-centered">
         <div className="flex flex-col md:flex-row md:gap-40 lg:gap-70 xl:gap-100 md:items-center md:mb-6">
-          {/* <GoBackBtn to="/locations" /> */}
+          <GoBackBtn to="/locations" />
 
           <p className="text-4xl text-center text-gray7 mb-6 md:mb-0">
             {locationObj.name}
