c454b2c43fb728ec3fdafb325b5dddf4e8a0f896
Eugene <eugenelevkovych@yahoo.com>
2025-08-10 13:46:04 +0300

    add svg

--- src/components/GoBackBtn.tsx
+++ src/components/GoBackBtn.tsx
@@ -9,7 +9,7 @@ export default function GoBackBtn({ to }: GoBackBtnProps) {
     <NavLink to={to}>
       <div className="flex items-center gap-2 font-bold text-lg text-black uppercase cursor-pointer">
         <svg className="size-6">
-          <use href="./sprite.svg#icon-arrow-back"></use>
+          <use href="/sprite.svg#icon-arrow-back"></use>
         </svg>
         <p>Go back</p>
       </div>
